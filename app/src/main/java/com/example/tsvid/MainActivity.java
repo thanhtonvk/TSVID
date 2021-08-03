5c2853d0fb4af361014d2e64cb7c07bd79078a94
Đỗ Tôn <67794492+thanhtonvk@users.noreply.github.com>
2021-08-03 15:22:26 +0700

    .

--- app/src/main/java/com/example/tsvid/MainActivity.java
+++ app/src/main/java/com/example/tsvid/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.tsvid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("mess");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                String value = snapshot.getValue(String.class);
+                Toast.makeText(getApplicationContext(),value,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled( DatabaseError error) {
+
+            }
+        });
     }
 }
